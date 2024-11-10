6037c8b5eed7f992baad3e8e946c1a85ab1d9e0f
hakusonmin <fujitakosei7@gmail.com>
2024-11-11 03:33:47 +0900

    9

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,26 @@
-// @ts-check
+import{ useState } from 'react';
+import{ useEffect } from 'react';
 
 export const DogListContainer = () => {
-  return <></>
+  const [breeds, setBreeds] = useState("https://dog.ceo/dog-api/");
+
+  const doHoge = async () => {
+    try{
+      const response = await fetch("https://dog.ceo/api/breeds/list/all"); 
+      const data = await response.json();
+      setBreeds(data.message); 
+    } catch (error) {
+      console.error("gige")
+    }
+  };
+
+  useEffect(() => {
+    doHoge();
+  },[]);
+
+  return <>
+    <img src={breeds}/>
+  </>
 }
 
 export default DogListContainer
